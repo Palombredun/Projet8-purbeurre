9785b4773f2db62d3fd397963577070c84e48670
baptiste <baptiste.name>
2019-07-14 16:27:09 +0200

    création des apps

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'ersatz',
+    'favourites',
+    'products',
+    'profiles',
+    'signup',
+    'users',
 ]
 
 MIDDLEWARE = [
