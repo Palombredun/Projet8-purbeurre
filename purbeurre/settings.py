217e2a73ea41b3f7620e1e3d46c9be2e746331c1
baptiste <baptiste.name>
2019-07-19 21:31:16 +0200

    Test Debug = False pour voir si le css s'affiche sur heroku

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&1uk#2&hy@(8o9lp%vtzr!@pe5$yai1d^^vv5hmtsq!gr)miyg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
