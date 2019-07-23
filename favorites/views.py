d05ba33af8bbdf1b202ee6d616e15378a8ba8e28
baptiste <baptiste.name>
2019-07-23 18:45:08 +0200

    Fonctionnalité favoris, vérifier favorites.html

--- favorites/views.py
+++ favorites/views.py
@@ -5,19 +5,26 @@ from django.contrib.auth.models import User
 from products.models import Product
 
 @login_required
-def favorites(request, id):
-    current_user = request.user
-    product = Product.objects.get(id=id)
-
-
+def favorites(request):
     if request.method == 'POST':
         current_user = request.user
         id_product = request.POST['product_id']
-        product = Product.objects.get(id=id_product)
-        new_favorite = User.favorites(product)
-        new_favorite.save()
+        product = Product.objects.get(id=id)
+        product.add(current_user)
     else:
         current_user = request.user
-        user = User.objects.get(id=current_user.id)
-        favorites = user.favorites.all()
-    return render(request, 'favorites/favorites.html', {'favorites': favorites})
+        favorites = Product.objects.filter(favorite__id=current_user.id)
+    return render(request, "favorites/favorites.html", {'favorites': favorites})
+
+
+    #if request.method == 'POST':
+    #    current_user = request.user
+    #    id_product = request.POST['product_id']
+    #    product = Product.objects.get(id=id_product)
+    #    new_favorite = User.favorites(product)
+    #    new_favorite.save()
+    #else:
+    #    current_user = request.user
+    #    user = User.objects.get(id=current_user.id)
+    #    favorites = user.favorites.all()
+    #return render(request, 'favorites/favorites.html', {'favorites': favorites})
